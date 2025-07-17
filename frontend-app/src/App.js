663b7cea318574665bd1527eb6fe2fcea1d76a36
Tauhidul Islam <testaccoffice3@gmail.com>
2025-07-17 11:04:25 +0600

    Add frontend-app as a normal folder

---
+++ frontend-app/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+         <h1>Welcome! This is Md.Tauhidul Islam's DevOps Exam App 🎓</h1>
+         <h4>Dipti Batch 4</h4>
+
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
